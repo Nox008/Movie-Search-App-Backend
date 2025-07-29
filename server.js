60ed31fb1f2a287e12ac8e7993a823bb3bafcfb8
Nox008 <anoxpg007@gmail.com>
2025-07-29 16:52:01 +0530

    Removed Trailing slash

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const corsOptions = {
       'http://127.0.0.1:5173',
       'http://localhost:4173',  // Vite preview
       'http://127.0.0.1:4173',
-      'https://movie-search-app-chi-five.vercel.app/'   //frontend
+      'https://movie-search-app-chi-five.vercel.app'   //frontend
     ];
     
     if (allowedOrigins.includes(origin)) {
