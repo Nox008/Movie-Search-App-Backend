c9e8647328bebff876bb345485e892e249593500
Nox008 <anoxpg007@gmail.com>
2025-07-29 16:47:48 +0530

    Updated Server.js

--- server.js
+++ server.js
@@ -21,7 +21,8 @@ const corsOptions = {
       'http://localhost:5173',  // Vite default
       'http://127.0.0.1:5173',
       'http://localhost:4173',  // Vite preview
-      'http://127.0.0.1:4173'
+      'http://127.0.0.1:4173',
+      'https://movie-search-app-chi-five.vercel.app/'   //frontend
     ];
     
     if (allowedOrigins.includes(origin)) {
